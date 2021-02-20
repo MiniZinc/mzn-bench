153ae92df687c2de2a46b30bfdc13eb3e6575cea
hbierlee <bierlee.henk@gmail.com>
2021-02-20 14:14:40 +1100

    Fix small code style inconsistency

--- src/mzn_bench/analysis/plot.py
+++ src/mzn_bench/analysis/plot.py
@@ -32,7 +32,7 @@ def plot_all_instances(
                     stats,
                     model,
                     data,
-                    palette=palette,
+                    palette,
                 )
                 for model in stats[stats.problem.eq(problem)].model.unique()
                 for data in stats[stats.problem.eq(problem)].data_file.unique()
